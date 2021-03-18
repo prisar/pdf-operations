d4a77245dc7962ef1486cdcc5c0dec95eba4a323
prisar <pritimaysarkar2010@gmail.com>
2021-03-18 17:50:52 +0530

    add drag and drop

--- src/containers/Pdf/MergePage/index.tsx
+++ src/containers/Pdf/MergePage/index.tsx
@@ -1,13 +1,24 @@
+import React from "react";
+import Dropzone from "react-dropzone";
+
 import "./index.css";
-import PdfView from '../../../components/PdfView';
-import PdfUpload from '../../../components/PdfUpload';
-import { Button } from 'react-bootstrap';
+import PdfView from "../../../components/PdfView";
+import PdfUpload from "../../../components/PdfUpload";
+import { Button } from "react-bootstrap";
 
 export function MergePage() {
-
+  const [firstfilename, setFirstFilename] = React.useState(null);
+  const [secondfilename, setSecondFilename] = React.useState(null);
   const merge = async () => {
     // merge api call
-  }
+  };
+
+  const processFiles = (acceptedFiles: any) => {
+    setFirstFilename(acceptedFiles[0].name);
+    if (acceptedFiles.length === 2) {
+      setSecondFilename(acceptedFiles[1].name);
+    }
+  };
 
   return (
     <div>
@@ -21,7 +32,25 @@ export function MergePage() {
       <Button variant="primary" onClick={merge}>
         Merge
       </Button>
-      {/* <PdfView divId="adobe-dc-view-1" location="http://localhost:8000/api/v1/pdf/download?file=reorderPagesInput.pdf" fileName="reorderPagesInput.pdf" /> */}
+
+      <Dropzone
+        onDrop={(acceptedFiles) => {
+          processFiles(acceptedFiles);
+        }}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div style={{ width: 1080, height: 100, backgroundColor: "gray", margin: 20, borderRadius: 10 }} {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+              <div>{firstfilename}</div>
+              <div>{secondfilename}</div>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+
+      <PdfView divId="adobe-dc-view-1" location="http://localhost:8000/api/v1/pdf/download?file=reorderPagesInput.pdf" fileName="reorderPagesInput.pdf" />
       {/* <PdfView divId="adobe-dc-view-2" location="https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" fileName="Bodea Brochure.pdf" /> */}
     </div>
   );
