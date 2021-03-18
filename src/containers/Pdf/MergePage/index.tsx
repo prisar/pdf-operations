7a889ef1872d79d9ff764fadd272daaf96d8bc1b
prisar <pritimaysarkar2010@gmail.com>
2021-03-18 08:25:23 +0530

    view

--- src/containers/Pdf/MergePage/index.tsx
+++ src/containers/Pdf/MergePage/index.tsx
@@ -17,8 +17,8 @@ export function MergePage() {
       <Button variant="primary" onClick={merge}>
         Merge
       </Button>
-      <PdfView divId="adobe-dc-view-1" location="http://localhost:8000/api/v1/pdf/download?file=3087.pdf" fileName="3087.pdf" />
-      {/* <PdfView divId="adobe-dc-view-2" location="https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" fileName="Bodea Brochure.pdf" /> */}
+      {/* <PdfView divId="adobe-dc-view-1" location="http://localhost:8000/api/v1/pdf/download?file=3087.pdf" fileName="3087.pdf" /> */}
+      <PdfView divId="adobe-dc-view-2" location="https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" fileName="Bodea Brochure.pdf" />
     </div>
   );
 }
