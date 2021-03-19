514d0d97ccdf95d40512358c09ddcd91499afdcb
prisar <pritimaysarkar2010@gmail.com>
2021-03-19 08:57:55 +0530

    show correct auth buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,94 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import './index.css';
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useHistory, withRouter } from "react-router-dom";
+
+import "./index.css";
+
+const AuthButton = styled.div`
+  position: absolute;
+  left: 87.5%;
+  right: 2.78%;
+  top: 18.75%;
+  bottom: 18.75%;
+`;
+const AuthButtonBg = styled.div`
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 5px;
+`;
+const AuthButtonTxt = styled.div`
+  position: absolute;
+  height: 25px;
+  left: 17px;
+  right: 16px;
+  top: calc(50% - 25px / 2 + 0.5px);
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 24px;
+  /* identical to box height */
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #000000;
+`;
 
 function Header() {
+  const [user, setUser] = React.useState(null);
+  const history = useHistory();
+
+  const login = () => {
+    history.push("/login");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("@user");
+    setUser(null);
+    history.push("/login");
+  };
+
   const authMenus = (
     <Fragment>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
           <Link className="nav-link" to="/">
-            <div className="link-text">Home{' '}</div>
+            <div className="link-text">Home </div>
             {/* <span className="sr-only">(current)</span> */}
           </Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/merge">
-          <div className="link-text">Merge</div>
+            <div className="link-text">Merge</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/split">
-          <div className="link-text">Split</div>
+            <div className="link-text">Split</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/delete">
-          <div className="link-text">Delete</div>
+            <div className="link-text">Delete</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/reorder">
-          <div className="link-text">Reorder</div>
+            <div className="link-text">Reorder</div>
           </Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/docsign">
-          <div className="link-text">eSign</div>
+            <div className="link-text">eSign</div>
           </Link>
         </li>
       </ul>
@@ -46,15 +100,15 @@ function Header() {
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
           <Link className="nav-link" to="/">
-          <div className="link-text">Home{' '}</div>
+            <div className="link-text">Home </div>
             {/* <span className="sr-only">(current)</span> */}
           </Link>
         </li>
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <Link className="nav-link" to="/about">
-          <div className="link-text">About</div>
+            <div className="link-text">About</div>
           </Link>
-        </li>
+        </li> */}
       </ul>
     </Fragment>
   );
@@ -63,14 +117,17 @@ function Header() {
     <Fragment>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <span className="nav-link active my-2 my-sm-0">
-            {/* {user ? user.first_name : ''} */}
-          </span>
+          <span className="nav-link active my-2 my-sm-0">{/* {user ? user.first_name : ''} */}</span>
         </li>
         <li className="nav-item">
-          <Link className="nav-link active my-2 my-sm-0" to="/login">
-          <div className="link-text">Logout</div>
-          </Link>
+          {/* <Link className="nav-link active my-2 my-sm-0" to="/login">
+            <div className="link-text">Logout</div>
+          </Link> */}
+          <AuthButton onClick={logout}>
+            <AuthButtonBg>
+              <AuthButtonTxt>Logout</AuthButtonTxt>
+            </AuthButtonBg>
+          </AuthButton>
         </li>
       </ul>
     </Fragment>
@@ -80,13 +137,39 @@ function Header() {
     <Fragment>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <Link className="nav-link active my-2 my-sm-0" to="/login">
-          <div className="link-text">Login</div>
-          </Link>
+          <AuthButton onClick={login}>
+            <AuthButtonBg>
+              <AuthButtonTxt>Login</AuthButtonTxt>
+            </AuthButtonBg>
+          </AuthButton>
         </li>
       </ul>
     </Fragment>
   );
+
+  const checkUser = () => {
+    const loggedInuser = localStorage.getItem("@user");
+    if (loggedInuser) {
+      setUser(JSON.parse(loggedInuser));
+    } else {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      console.log("user");
+      checkUser();
+    }
+
+    history.listen((location: any, action: any) => {
+      console.log(action, location.pathname, location.state);
+      if (!user) {
+        console.log("user");
+        checkUser();
+      }
+    });
+  }, [user]);
 
   return (
     <div>
@@ -94,27 +177,17 @@ function Header() {
         <Link className="navbar-brand" to="/">
           <div className="app-name">PDF Operations</div>
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor01"
-          aria-controls="navbarColor01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
 
         <div className="collapse navbar-collapse" id="navbarColor01">
-          {true ? authMenus : guestMenus}
-          <div className="form-inline my-2 my-lg-0">
-            {true ? authLinks : guestLinks}
-          </div>
+          {user ? authMenus : guestMenus}
+          <div className="form-inline my-2 my-lg-0">{user ? authLinks : guestLinks}</div>
         </div>
       </nav>
     </div>
   );
 }
 
-export default Header;
+export default withRouter(Header);
--- src/containers/Auth/LoginPage/index.tsx
+++ src/containers/Auth/LoginPage/index.tsx
@@ -1,35 +1,166 @@
 import React from "react";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import Dropzone from "react-dropzone";
+import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 
 import constants from "../../../config/constants";
 
+const ContainerBox = styled.div`
+  position: relative;
+  width: 1440px;
+  height: 700px;
+  left: 0px;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  width: 1440px;
+  height: 700px;
+  left: 0px;
+  top: 0px;
+
+  background: #f2f2f2;
+`;
+
+const CustomBtn = styled.div`
+  position: absolute;
+  left: 37.5%;
+  right: 37.5%;
+  top: 64.08%;
+  bottom: 24.65%;
+
+  background: #000000;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const CustomBtnText = styled.div`
+  position: absolute;
+  height: 22px;
+  left: 25px;
+  right: 24px;
+  top: calc(50% - 22px / 2);
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 32px;
+  line-height: 21px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+const Title = styled.div`
+  position: absolute;
+  left: 35.35%;
+  right: 35.35%;
+  top: 21.13%;
+  bottom: 73.77%;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 64px;
+  line-height: 52px;
+  /* identical to box height */
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #000000;
+`;
+
+const EmailInput = styled.input`
+  position: absolute;
+  left: 37.5%;
+  right: 37.5%;
+  top: 34.51%;
+  bottom: 54.23%;
+`;
+
+const EmailPlaceholder = styled.div`
+  position: absolute;
+  left: 39.24%;
+  right: 57.36%;
+  top: 38.03%;
+  bottom: 56.87%;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 28px;
+  /* identical to box height */
+
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const PasswordInput = styled.input`
+  position: absolute;
+  left: 37.5%;
+  right: 37.5%;
+  top: 48.59%;
+  bottom: 40.14%;
+`;
+
+const PasswordPlaceholder = styled.div`
+  position: absolute;
+  left: 39.24%;
+  right: 54.65%;
+  top: 51.94%;
+  bottom: 42.96%;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 28px;
+  /* identical to box height */
+
+  color: rgba(0, 0, 0, 0.5);
+`;
+
 export function LoginPage() {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [username, setUsername] = React.useState(null);
+  const [password, setPassword] = React.useState(null);
+  const [error, setError] = React.useState("");
   const history = useHistory();
 
   const onSubmitForm = async () => {
-    const data = {
-      username,
-      password,
-    };
-    const url = `${constants.backend}/api/v1/auth/login`;
-    const response = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      // mode: 'cors', // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      // credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      // redirect: 'follow', // manual, *follow, error
-      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    console.log(response.json());
-    history.push("/");
+    try {
+      const data = {
+        username,
+        password,
+      };
+      console.log(data);
+      const url = `${constants.backend}/api/v1/auth/login`;
+      const response = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // mode: 'cors', // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+      });
+
+      if (response.status === 200) {
+        const user = await response.json();
+        localStorage.setItem("@user", JSON.stringify(user.user));
+        history.push("/");
+      } else {
+        setError("Login error");
+      }
+    } catch (err) {
+      setError(err);
+    }
   };
 
   const onChangeUsername = (event: any) => {
@@ -40,35 +171,26 @@ export function LoginPage() {
     setPassword(event.target.value);
   };
 
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+  }, [username, password]);
 
   return (
     <div>
-      <Container>
-        <Row>
-          <Col />
-          <Col className="login-col">
-            <div className="text-center">Login</div>
-            {/* <Form onSubmit={onSubmitForm}> */}
-            <Form>
-              <Form.Group controlId="formUsername">
-                <Form.Label>username</Form.Label>
-                <Form.Control placeholder="Enter username" onChange={onChangeUsername} />
-              </Form.Group>
+      <ContainerBox>
+        <Container>
+          {/* <Title>Login</Title> */}
+          <EmailInput type="text" onChange={onChangeUsername}></EmailInput>
+          {!username && <EmailPlaceholder>email</EmailPlaceholder>}
+          <PasswordInput type="password" onChange={onChangePassword}></PasswordInput>
+          {!password && <PasswordPlaceholder>password</PasswordPlaceholder>}
 
-              <Form.Group controlId="formPassword">
-                <Form.Label>password</Form.Label>
-                <Form.Control type="password" placeholder="Password" onChange={onChangePassword} />
-              </Form.Group>
-              {/* <Button variant="primary" type="submit"> */}
-              <Button variant="primary" onClick={onSubmitForm}>
-                Submit
-              </Button>
-            </Form>
-          </Col>
-          <Col />
-        </Row>
-      </Container>
+          <CustomBtn onClick={onSubmitForm}>
+            <CustomBtnText>Login</CustomBtnText>
+          </CustomBtn>
+
+          {error && <div style={{ fontSize: 18, color: "#000", margin: 20, position: "absolute", left: "39.24%", right: "54.65%", top: "71.94%" }}>{error.toString()}</div>}
+        </Container>
+      </ContainerBox>
     </div>
   );
 }
