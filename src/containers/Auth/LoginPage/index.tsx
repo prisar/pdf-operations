2ac3485536b0a4624493903d75085f5ebb340700
prisar <pritimaysarkar2010@gmail.com>
2021-03-22 22:38:51 +0530

    clean

--- src/containers/Auth/LoginPage/index.tsx
+++ src/containers/Auth/LoginPage/index.tsx
@@ -138,17 +138,11 @@ export function LoginPage() {
       console.log(data);
       const url = `${constants.backend}/api/v1/auth/login`;
       const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        // mode: 'cors', // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'same-origin', // include, *same-origin, omit
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        // redirect: 'follow', // manual, *follow, error
-        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        body: JSON.stringify(data),
       });
 
       if (response.status === 200) {
